c2925bcd148caca23d8deb659f5eeb538fa4f08a
SeoJinhye <jyoon@sjnma.org>
2017-02-22 10:43:31 -0600

    [Problem1 done]

---
+++ Problem1.py
@@ -0,0 +1,9 @@
+print("type three number")
+a = int(raw_input(">"))
+b = int(raw_input(">"))
+c = int(raw_input(">"))
+
+i = a+b
+
+if(i == c):
+	print("true")
---
+++ Problem2.py
@@ -0,0 +1,16 @@
+print("type three number")
+a1 = int(raw_input(">"))
+a2 = int(raw_input(">"))
+a3 = int(raw_input(">"))
+
+if(a1 < a2< a3):
+	print("Up and risying(?)")
+
+if(a3 < a2 < a1):
+	print("Falling Down")
+
+if(a3 < a2 && a1 < a2):
+	print("Over the mountain")
+
+if(if a2 < a3 && a2< a1):
+	print("in the dog house")
